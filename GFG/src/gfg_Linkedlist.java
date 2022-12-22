2d6b8f012d92163621ba3616540819bdbf4bade3
Saroj Kumar Jena <80943516+saroj379@users.noreply.github.com>
2022-12-22 23:17:18 +0530

    linkedlist function to gfg

---
+++ GFG/src/gfg_Linkedlist.java
@@ -0,0 +1,73 @@
+/*
+LinkedList of geeks for geeks (as diction sequence)
+Totall Problem :- 3
+*/
+
+class Node{
+    int data;
+    Node next;
+    Node(int d){
+        this.data = d;
+        this.next = null;
+    }
+}
+public class gfg_Linkedlist {
+
+    //Detect lope in linkedlist.
+    public static boolean detectLoop(Node head){
+        Node slow = head;
+        Node fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    //Finding middle element in a linked list
+    int getMiddle(Node head)
+    {
+        Node tor = head;
+        Node hare = head;
+        while(hare != null && hare.next != null){
+            tor = tor.next;
+            hare = hare.next.next;
+        }
+        return tor.data;
+    }
+
+
+    //Remove a loop in the linked list.
+    public static void removeLoop(Node head){
+        Node slow = head;
+        Node fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast){
+                slow = head;
+                if(slow == fast){
+                    while(fast.next != slow){
+                        fast = fast.next;
+                    }
+                }else{
+                    while(fast.next != slow.next){
+                        slow = slow.next;
+                        fast = fast.next;
+                    }
+                }
+                fast.next = null;
+            }
+        }
+    }
+
+
+
+}
