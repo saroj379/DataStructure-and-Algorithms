3a49ff512bb90d77fd0e5654d07ecd0eacc083ec
Saroj Kumar Jena <80943516+saroj379@users.noreply.github.com>
2023-02-07 18:32:59 +0530

    hollow_rectangle And Butterfly patterns

---
+++ Apna_College/Alpha Batch 3.0/Patterns_Part-II_Advance/src/Main.java
@@ -0,0 +1,63 @@
+
+public class Main
+{
+    public static void main(String[] args) {
+        int row = 4, col = 6;
+        // 1- Hollow Rectangle
+        for(int i=1; i <= row; i++){
+            for(int j=1; j<= col; j++){
+                if(i == 1 || j== col || j == 1 || i ==row){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+
+
+        // 2  Alternative 0,1's
+        for(int i=1; i <= row; i++){
+            for(int j=1; j<= i; j++){
+                if((i+j) % 2 == 0){
+                    System.out.print(1 +" ");
+                }
+                else{
+                    System.out.print(0+" ");
+                }
+            }
+            System.out.println();
+        }
+
+
+        // 3 Butterfly Pattern
+        int n = 4;
+        // 1st half
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            for(int j = 1; j <= 2*(n-i); j++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        // 2nd half
+        for(int i = n; i >= 1; i--){
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            for(int j = 1; j <= 2*(n-i); j++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
