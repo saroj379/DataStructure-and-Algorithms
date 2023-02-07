1dfb62863d05ac323d421e9ef60cc15f8d09e10f
Saroj Kumar Jena <80943516+saroj379@users.noreply.github.com>
2023-02-07 23:54:47 +0530

    Diamond Pattern

--- Apna_College/Alpha Batch 3.0/Patterns_Part-II_Advance/src/Main.java
+++ Apna_College/Alpha Batch 3.0/Patterns_Part-II_Advance/src/Main.java
@@ -89,5 +89,29 @@ public class Main
             }
             System.out.println();
         }
+
+
+        // Diamond Pattern
+        n = 5;
+        // 1st half
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n-i; j++){
+                System.out.print(" ");
+            }
+            for(int j =1; j < 2*i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        // 2nd half
+        for(int i = n; i >= 1; i--){
+            for(int j = 1; j <= n-i; j++){
+                System.out.print(" ");
+            }
+            for(int j =1; j < 2*i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
