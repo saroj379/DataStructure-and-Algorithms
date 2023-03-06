4e17dbef65df614e03da15935db138e012dc0508
Saroj Kumar Jena <80943516+saroj379@users.noreply.github.com>
2023-03-06 15:49:59 +0530

    set ith bit

--- Apna_College/Alpha Batch 3.0/Bit Manipulation/Basics_bit_wise_operator/src/Main.java
+++ Apna_College/Alpha Batch 3.0/Bit Manipulation/Basics_bit_wise_operator/src/Main.java
@@ -11,7 +11,8 @@ public class Main {
 
     // get ith bit
     public static int get_ithBit(int n, int i){
-        if((n & (1 << i)) == 0){
+        int bitMask = 1 << i;
+        if((n & bitMask) == 0){
             return 0;
         }
         else{
@@ -19,16 +20,23 @@ public class Main {
         }
     }
 
-    //
-    
+    // set ith bit
+    public static int set_ithBit(int n, int i){
+        int bitMask = 1 << i;
+        return n | bitMask;
+    }
+
     public static void main(String[] args) {
         // Odd or Even using bit-wise operator
         oddOrEven(3);
         oddOrEven(18);
 
         // get ith bit
-        System.out.println(get_ithBit(15, 2));
-        System.out.println(get_ithBit(10, 2));
-        System.out.println(get_ithBit(15, 3));
+        System.out.println("get ith bit : "+get_ithBit(15, 2));
+        System.out.println("get ith bit : "+get_ithBit(10, 2));
+        System.out.println("get ith bit : "+get_ithBit(15, 3));
+
+        // set ith bit
+        System.out.println("Set ith bit : "+set_ithBit(10, 2));
     }
 }
