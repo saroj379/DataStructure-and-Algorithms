732e6ef4c9e9959008e48643aa6b787dfa92e3f0
Saroj Kumar Jena <80943516+saroj379@users.noreply.github.com>
2023-03-10 17:14:51 +0530

    Minimum in BST

---
+++ Accio/Module-4_Intermediate_DSA/M4_Medium/Minimum element in BST/src/Main.java
@@ -0,0 +1,72 @@
+import java.util.*;
+import java.io.*;
+
+class Node{
+    int val;
+    Node left, right;
+    Node(int val){
+        this.val = val;
+        left = null;
+        right = null;
+    }
+}
+class BST{
+    Node root = null;
+    BST(){
+
+    }
+    BST(Node root){
+        root=root;
+    }
+    Node insert(Node root, int val){
+        if(root == null){
+            root = new Node(val);
+            return root;
+        }
+        if(root.val == val)
+            return root;
+        if(val < root.val){
+            root.left = insert(root.left, val);
+        }else{
+            root.right = insert(root.right, val);
+        }
+        return root;
+    }
+    void print(Node root){
+        System.out.print(root.val + " ");
+        if(root.left != null){
+            print(root.left);
+        }
+        if(root.right != null){
+            print(root.right);
+        }
+    }
+}
+
+class Solution{
+    public int minimumNode(Node root){
+        if(root.left == null && root.right == null)  return root.val;
+        else if(root.left == null) return root.val;
+        else{
+            Node temp = root;
+            while(temp.left != null)
+                temp = temp.left;
+            return temp.val;
+        }
+    }
+}
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        BST t = new BST();
+        for(int i = 0; i < n; i++){
+            t.root = t.insert(t.root, input.nextInt());
+        }
+        Solution obj=new Solution();
+        int result= obj.minimumNode(t.root);
+        System.out.print(result);
+
+    }
+}
