372796b560dc935dd6c95fe66d4b84ed3df3afcb
Saroj Kumar Jena <80943516+saroj379@users.noreply.github.com>
2023-03-31 01:01:51 +0530

    Complete All of recursion-1 of Coding Bat

--- CodingBat_Java/Recursion-1/src/Main.java
+++ CodingBat_Java/Recursion-1/src/Main.java
@@ -186,8 +186,132 @@ class Solutions{
         return countPairs(str.substring(1));
     }
 
+    // countAbc
+    public int countAbc(String str) {
+        if(str.length() <=2) return 0;
+        if(str.substring(0,3).equals("abc") || str.substring(0,3).equals("aba")){
+            return 1 + countAbc(str.substring(1));
+        }
+        return countAbc(str.substring(1));
+    }
 
+    // count11
+    public int count11(String str) {
+        if(str.length() < 2) return 0;
+        if(str.substring(0,2).equals("11")){
+            return 1 + count11(str.substring(2));
+        }
+        return count11(str.substring(1));
+    }
 
+    // stringClean
+    public String stringClean(String str) {
+        if(str.length() <= 1) return str;
+        if(str.charAt(0) == str.charAt(1)){
+            return stringClean(str.substring(1));
+        }
+        return str.charAt(0) + stringClean(str.substring(1));
+    }
+
+    // countHi2
+    public int countHi2(String str) {
+        if(str.length() < 2) return 0;
+        if(str.charAt(0) == 'x'){
+            if(str.length() > 2 && str.substring(1,3).equals("hi")){
+                return countHi2(str.substring(2));
+            }
+            else{
+                return countHi2(str.substring(1));
+            }
+        }else if(str.substring(0,2).equals("hi")){
+
+            return 1 + countHi2(str.substring(1));
+        }
+        return countHi2(str.substring(1));
+
+    }
+
+    // optimized code
+    public int countHi2_(String str) {
+        int len = str.length();
+        if(len < 2) return 0;
+        if(str.substring(len-2, len).equals("hi")){
+            if(len >2 && str.charAt(len-3)!='x' || len==2){
+                return 1 + countHi2(str.substring(0, len-1));
+            }
+        }
+        return countHi2(str.substring(0, len-1));
+    }
+
+    // parenBit
+    public String parenBit(String str) {
+        if(str.equals("")) return str;
+        if(str.charAt(0) == '('){
+            if(str.charAt(str.length()-1) == ')'){
+                return str;
+            }else{
+                return parenBit(str.substring(0, str.length()-1));
+            }
+        }
+        return parenBit(str.substring(1));
+    }
+
+    // nestParen
+    public boolean nestParen(String str) {
+        int len = str.length();
+        if(len == 0) return true;
+        if(str.charAt(0) == '(' && str.charAt(len-1) == ')'){
+            return nestParen(str.substring(1, len -1));
+        }
+        return false;
+    }
+
+    // strCount
+    public int strCount(String str, String sub) {
+        int lenSub = sub.length();
+        int len = str.length();
+        if(len < lenSub) return 0;
+        if(str.substring(len-lenSub, len).equals(sub)){
+            return 1 + strCount(str.substring(0, len-lenSub), sub);
+        }
+        return strCount(str.substring(0, len-1), sub);
+    }
+
+    // strCopies
+    public boolean strCopies(String str, String sub, int n) {
+        int lenSub = sub.length();
+        int len = str.length();
+        if(len < lenSub){
+            if(n==0) return true;
+            else return false;
+        }
+        if(len >= lenSub && str.substring(len-lenSub, len).equals(sub)){
+            return strCopies(str.substring(0, len-1), sub, n-1);
+        }
+        return strCopies(str.substring(0, len-1), sub, n);
+    }
+
+    // strDist
+    public int strDist(String str, String sub) {
+        return func(str,sub).length();
+    }
+
+    private String func(String str, String sub) {
+        int lenSub = sub.length();
+        int len = str.length();
+
+        if(str.equals("")) return str;
+        if(str.startsWith(sub)){
+            if(str.substring(len-lenSub, len).equals(sub)){
+                return str;
+            }
+            else{
+                return func(str.substring(0, len-1), sub);
+            }
+        }
+        return func(str.substring(1, len), sub);
+    }
+    
 }
 
 
