348ebd91c4e0f7b9794afcb9f2d82c81f863e5e3
Saroj Kumar Jena <80943516+saroj379@users.noreply.github.com>
2023-04-04 10:10:36 +0530

    Find length of Loop

---
+++ GFG/Easy_/Find length of Loop/src/Main.java
@@ -0,0 +1,111 @@
+//{ Driver Code Starts
+// driver code
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Node
+{
+    int data;
+    Node next;
+
+    Node(int x)
+    {
+        data = x;
+        next = null;
+    }
+}
+
+class GFG
+{
+    public static void makeLoop(Node head, Node tail, int x){
+        if (x == 0) return;
+
+        Node curr = head;
+        for(int i=1; i<x; i++)
+            curr = curr.next;
+
+        tail.next = curr;
+    }
+
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while(t--> 0)
+        {
+            int n = sc.nextInt();
+
+            int num = sc.nextInt();
+            Node head = new Node(num);
+            Node tail = head;
+
+            for(int i=0; i<n-1; i++)
+            {
+                num = sc.nextInt();
+                tail.next = new Node(num);
+                tail = tail.next;
+            }
+
+            int pos = sc.nextInt();
+            makeLoop(head, tail, pos);
+
+            Solution x = new Solution();
+            System.out.println( x.countNodesinLoop(head) );
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/*
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) {data = d; next = null; }
+}
+
+*/
+
+//Function should return the length of the loop in LL.
+
+class Solution
+{
+    //Function to find the length of a loop in the linked list.
+    static int countNodesinLoop(Node head)
+    {
+        // base case
+        if(head == null && head.next == null) return 0;
+        // Detect the loop 1st
+        Node tor = head;
+        Node hear = head;
+        boolean pre = false;
+        while(hear != null && hear.next != null){
+
+            tor = tor.next;
+            hear = hear.next.next;
+
+            if(tor == hear){
+                pre = true;
+                break;
+            }
+        }
+
+        int countLen =1;
+
+        if(pre == false) return 0;
+        else{
+            hear = hear.next;
+            while(hear != tor){
+                hear = hear.next;
+                countLen++;
+            }
+        }
+
+        return countLen;
+    }
+}
