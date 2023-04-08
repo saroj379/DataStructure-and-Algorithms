b13f9ac489e885bed44c9fd4b76a677a8d98d36e
Saroj Kumar Jena <80943516+saroj379@users.noreply.github.com>
2023-04-09 02:10:26 +0530

    Merge 2 sorted linked list in reverse order

---
+++ GFG/Easy_/Merge 2 sorted linked list in reverse order/src/Main.java
@@ -0,0 +1,149 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+class Node
+{
+    int data;
+    Node next;
+
+    Node(int d)
+    {
+        data = d;
+        next = null;
+    }
+}
+
+
+class Merge_2_sorted_ll_In_ReverseOrder
+{
+    Node head;
+
+    void addToTheLast(Node node)
+    {
+        if(head == null)
+            head = node;
+        else
+        {
+            Node temp = head;
+            while(temp.next != null)
+                temp = temp.next;
+            temp.next = node;
+        }
+    }
+
+    void printList(Node node)
+    {
+        while(node!= null)
+        {
+            System.out.print(node.data+ " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+    public static void main(String args[]) throws IOException
+    {
+        //Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while(t>0)
+        {
+            String nums[] = br.readLine().split(" ");
+            int n1 = Integer.parseInt(nums[0]);
+            int n2 = Integer.parseInt(nums[1]);
+            Merge_2_sorted_ll_In_ReverseOrder list = new Merge_2_sorted_ll_In_ReverseOrder();
+            nums = br.readLine().split(" ");
+            int a1 = Integer.parseInt(nums[0]);
+            Node head = new Node(a1);
+            list.addToTheLast(head);
+            for(int i=1;i<n1;i++)
+            {
+                int a = Integer.parseInt(nums[i]);
+                list.addToTheLast(new Node(a));
+            }
+            //list.printList(list.head);
+
+            Merge_2_sorted_ll_In_ReverseOrder list2 = new Merge_2_sorted_ll_In_ReverseOrder();
+            nums = br.readLine().split(" ");
+            int b1 = Integer.parseInt(nums[0]);
+            Node head1 = new Node(b1);
+            list2.addToTheLast(new Node(b1));
+            for(int j=1;j<n2;j++)
+            {
+                int b = Integer.parseInt(nums[j]);
+                list2.addToTheLast(new Node(b));
+            }
+            //list.printList(list2.head);
+
+            GfG g = new GfG();
+            Node result = g.mergeResult(list.head, list2.head);
+            list.printList(result);
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/* Structure of the node*/
+/* class Node
+{
+	int data;
+	Node next;
+	
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+} */
+
+class GfG
+{
+    // to reverse the ll
+    Node reverseList(Node head){
+        Node prev = null;
+        Node curr = head;
+        while(curr != null){
+            Node nxt = curr.next;
+
+            // changing the pointer
+            curr.next = prev;
+
+            // Updating 
+            prev = curr;
+            curr = nxt;
+        }
+        return prev;
+    }
+
+    Node mergeResult(Node node1, Node node2)
+    {
+        Node head1 = reverseList(node1);
+        Node head2 = reverseList(node2);
+
+        // to iterate created 2 pointer on the reverser ll
+        Node h1 = head1;
+        Node h2 = head2;
+
+        Node dummy = new Node(-1);
+        Node curr = dummy;// it will point to the newly created ll
+
+        while(h1 != null && h2 != null){
+            if(h1.data > h2.data){
+                curr.next = h1;
+                h1 = h1.next;
+            }else{
+                curr.next = h2;
+                h2 = h2.next;
+            }
+            curr = curr.next;
+        }
+
+        // after this check h1 is present or h2 is present
+        if(h1 != null) curr.next = h1;
+        if(h2 != null) curr.next = h2;
+
+        return dummy.next;
+    }
+}
