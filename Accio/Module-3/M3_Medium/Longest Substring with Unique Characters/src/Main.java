25b524cebb6286e9cc126cbf9cd7a0deb0b03855
Saroj Kumar Jena <80943516+saroj379@users.noreply.github.com>
2023-05-20 00:58:38 +0530

    Longest Substring with Unique Characters

---
+++ Accio/Module-3/M3_Medium/Longest Substring with Unique Characters/src/Main.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public int longestSubstring(String s) {
+        Map<Character, Integer> chars = new HashMap();
+
+        int left = 0;
+        int right = 0;
+
+        int res = 0;
+        while (right < s.length()) {
+            char r = s.charAt(right);
+            chars.put(r, chars.getOrDefault(r,0) + 1);
+
+            while (chars.get(r) > 1) {
+                char l = s.charAt(left);
+                chars.put(l, chars.get(l) - 1);
+                left++;
+            }
+
+            res = Math.max(res, right - left + 1);
+
+            right++;
+        }
+        return res;
+    }
+}
+
+public class Main {
+    public static void main (String[] args) throws java.lang.Exception {
+        Scanner sc=new Scanner(System.in);
+        String s = sc.nextLine();
+        Solution ob = new Solution();
+        int ans=ob.longestSubstring(s);
+        System.out.println(ans);
+    }
+}
