6a8973808204458bff091749d638635598de5217
Saroj Kumar Jena <80943516+saroj379@users.noreply.github.com>
2023-07-16 01:20:08 +0530

    deleted file

--- Accio/MixBag_Problems_PlacementReadyness/xyz/main.java
+++
@@ -1,7 +0,0 @@
-package DSA__JAVA.Accio.MixBag_Problems_PlacementReadyness.xyz;
-
-public class main {
-    public static void main(String[] args) {
-        System.out.print("Yes, I am also added successfully!");
-    }
-}
