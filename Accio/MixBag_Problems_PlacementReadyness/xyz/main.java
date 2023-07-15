99cc8cc9c0691a5750f30ef13c331b3ba98ea301
Saroj Kumar Jena <80943516+saroj379@users.noreply.github.com>
2023-07-16 01:19:20 +0530

    new file

---
+++ Accio/MixBag_Problems_PlacementReadyness/xyz/main.java
@@ -0,0 +1,7 @@
+package DSA__JAVA.Accio.MixBag_Problems_PlacementReadyness.xyz;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.print("Yes, I am also added successfully!");
+    }
+}
